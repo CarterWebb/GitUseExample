a7a0a8753113394c332e61019fa50ce9e81357d4
Webb, Carter J <140331@s020rmc148-17.local>
2018-11-14 13:29:55 -0600

    Added new class for map

---
+++ GitUseExample/MapForItinerary.swift
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.swift
+//  GitUseExample
+//
+//  Created by Webb, Carter J on 11/14/18.
+//  Copyright © 2018 Webb, Carter J. All rights reserved.
+//
+
+import Foundation
+class MapForItinerary: NSObject
+{
+    
+}
